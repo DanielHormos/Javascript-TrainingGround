25c73bc430aba90c9b7cb0c1d3eea895c3e9eb4d
Daniel Hormos <daniel_danne32@hotmail.com>
2024-09-19 19:33:07 +0200

    added function that creates a unordered list of elements

--- functions.js
+++ functions.js
@@ -2,5 +2,19 @@ myFunctions = {
     getCompanyName: function (){
         return "Salt"
     }
+    ,
+    addListInMain: function (numberOfListItems) {
+        const ulElement = document.createElement('ul')
+
+        for(let i = 0; i < numberOfListItems; i++){
+            const liItem = document.createElement("li")
+            liItem.textContent = `Item #${i + 1}`
+
+            ulElement.appendChild(liItem)
+        }
+    
+        const main = document.querySelector("main")
+        main.appentChild(ulElement)
+    }
 }
 console.log("salt> Functions loaded")
